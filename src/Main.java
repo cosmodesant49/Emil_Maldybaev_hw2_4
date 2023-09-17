fb804da250776db86d9649e43502edf211a28090
cosmodesant49 <emilmaldybaev49@gmail.com>
2023-09-17 21:40:36 +0600

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,38 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Comparator;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        ArrayList<String> listA = new ArrayList<String>();
+        int size = listA.size();
+        Scanner scanner = new Scanner(System.in );
+        while(listA.size()<5) {
+            System.out.println("type 5 name for list A");
+            String number = scanner.nextLine();
+            listA.add(number);
         }
+        System.out.println("лист А"+ listA);
+        ArrayList<String> listB = new ArrayList<String>();
+        int size2 = listB.size();
+        Scanner scanner2 = new Scanner(System.in );
+        while(listB.size()<5) {
+            System.out.println("type 5 name for list B");
+            String number = scanner.nextLine();
+            listB.add(number);
+        }
+        System.out.println("лист А"+ listA);
+        System.out.println("лист B"+ listB);
+        ArrayList<String> listC = new ArrayList<String>();
+        for (int i = 0; i < listA.size(); i++) {
+            listC.add(listA.get(i));
+            listC.add(listB.get(4 - i));
+        }
+
+        System.out.println("лист С" + listC);
+
+        Collections.sort(listC, Comparator.comparingInt(String::length));
+        System.out.println("отсортированный лит С "+listC);
     }
 }
